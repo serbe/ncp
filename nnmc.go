89d84e54e5e8c970d425bf26e406cca25b72f374
Valera <mkoctuk@gmail.com>
2016-01-14 09:30:30 +0300

    all in one file

--- nnmc.go
+++ nnmc.go
@@ -1,11 +1,25 @@
 package nnmc
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"golang.org/x/net/html/charset"
 )
+
+// NNMc values:
+// client http.Client with cookie
+type NNMc struct {
+	client http.Client
+}
 
 // Topic from forum
 type Topic struct {
@@ -67,6 +81,46 @@ type Film struct {
 	Torrent       string
 	Poster        string
 	Hide          bool
+}
+
+// Init nnmc with login password
+func Init(login string, password string) (*NNMc, error) {
+	var client http.Client
+	cookieJar, _ := cookiejar.New(nil)
+	client.Jar = cookieJar
+	urlPost := "http://nnm-club.me/forum/login.php"
+	form := url.Values{}
+	form.Set("username", login)
+	form.Add("password", password)
+	form.Add("redirect", "")
+	form.Add("login", "âõîä")
+	req, _ := http.NewRequest("POST", urlPost, bytes.NewBufferString(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+	_, err := client.Do(req)
+	return &NNMc{client: client}, err
+}
+
+// getHTML get body from url
+func getHTML(url string, n *NNMc) ([]byte, error) {
+	resp, err := n.client.Get(url)
+	if err != nil {
+		log.Println("client Get error:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	utf8body, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
+	if err != nil {
+		log.Println("Encoding error:", err)
+		return nil, err
+	}
+	doc, err := ioutil.ReadAll(utf8body)
+	if err != nil {
+		log.Println("ioutil.ReadAll error:", err)
+	}
+	doc = replaceAll(doc, "&nbsp;", " ")
+	doc = replaceAll(doc, "&amp;", "&")
+	return doc, nil
 }
 
 // ParseForumTree get topics from forumTree
